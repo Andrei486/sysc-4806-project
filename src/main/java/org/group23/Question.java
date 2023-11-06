6490d60a8bbdbe52f5d7dd9761ebb62812df5c4b
CamSommer <CamSommerville@cmail.carleton.ca>
2023-11-06 13:05:07 -0500

    Initial Commit

---
+++ src/main/java/org/group23/Question.java
@@ -0,0 +1,9 @@
+package org.group23;
+
+public class Question {
+
+    public int  survey_id;
+    public int id;
+    public String question;
+
+}
---
+++ src/main/java/org/group23/Survey.java
@@ -0,0 +1,10 @@
+package org.group23;
+
+import java.util.ArrayList;
+
+public class Survey {
+
+    public int id;
+    public ArrayList<Question> questions;
+
+}
