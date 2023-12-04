032e53e7609b52aed29513690663412b5dfe31e0
Andrei Popescu <apopescu486@gmail.com>
2023-12-04 13:30:37 -0500

    Added button to remove options

--- src/main/resources/static/js/addRemoveMcOptions.js
+++ src/main/resources/static/js/addRemoveMcOptions.js
@@ -1,14 +1,14 @@
-let container;
-let cloneOption;
-
-document.addEventListener("DOMContentLoaded", function () {
-    container = document.getElementById("mcOptions");
-    cloneOption = document.getElementById("firstMcOption");
-});
-
-
 function addMcOption() {
+    const container = document.getElementById("mcOptions");
+    const cloneOption = document.getElementById("firstMcOption");
     const clonedOption = cloneOption.cloneNode(true);
     container.append(clonedOption);
+}
+
+function removeMcOption() {
+    const container = document.getElementById("mcOptions");
+    if (container.children.length > 1) {
+        container.removeChild(container.lastChild);
+    }
 }
 
